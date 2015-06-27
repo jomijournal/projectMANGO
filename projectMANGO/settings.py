2f90aa06f9f90023ba5986731418097a83befd8d
Christian Testa <christian.testa@jomi.com>
2015-06-27 16:31:22 -0400

    configured frontpage base.html

--- projectMANGO/settings.py
+++ projectMANGO/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'core',
     'journal',
+    'frontpage',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'projectMANGO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projectMANGO/urls.py
+++ projectMANGO/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('frontpage.urls')),
     url(r'^article/', include('journal.urls')),
     url(r'articles/', include('all_articles.urls')),
     url(r'^admin/', include(admin.site.urls)),
