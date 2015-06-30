bd3fa081b7b14a46e05ffa8d0a327fe76a362dc6
rusty122 <russell.parker7@gmail.com>
2015-06-30 11:21:29 -0400

    Pasted all of the models that we designed yesterday

--- core/models.py
+++ core/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+'''
+Institution: Holds the data associated with each institution. Institutions will
+             be added as new hospitals and libraries register.
+'''
+class Institution(users.Model)
+    name = models.CharField(max_length = 64,
+           help_text = 'Name of the institution(e.g. Massachusetts General Hospital).')
+    location = CharField(max_length = 256, help_text = 'Where the institution is located')
+    IPrange = models.TextField(help_text = 'Range of the IP addresses associated with the institution.')
+    # necessary?
+    pointsOfContacts = models.ManyToOneField(Account,
+             help_text = 'Librarians or other users who should have special access.\
+             This will likely include access to Counter reports.')
+
+
+'''
+User: Holds the data associated with each user registered on the website.
+'''
+class Account(models.Model):
+    # Discuss choices with Bizdev and Editorial
+    TYPES = (
+            (1, 'Doctor'),
+            (2, 'Resident'),
+            (3, 'Medical Student'),
+            (4, 'Librarian'),
+            (5, 'Attending'),
+            (6, 'Premed'),
+            (7, 'Veterinarian'),
+            (8, 'Other')
+    )
+    user = models.OneToOneField(User)
+    accountType = models.IntegerType( choice = TYPES, default = 8,
+                  help_text = 'The account type which affects what a user can do.\
+                             This is mostly for JoMI\'s internal records because the\
+                             User type already includes a group that we can define.')
+    institution = models.ForeignKey(Institution,
+                  help_text = 'Institution associated with the user.')
+    # social login ??
+
+
+'''
+Author: Class inherits from User. The Author class additionally
+        includes also includes a link to the individual\'s picture, their
+        specialty, and their position.
+'''
+class Author(models.Model):
+    account = models.ForeignKey(Account)
+    displayName = models.CharField(max_length = 32, help_text = 'The user\'s nickname.')
+    image = models.URLField(help_text = 'A link to an image of the author.')
+    specialty = models.CharField(max_length = 64,
+                help_text = 'The author\'s specialty.')
+    position = models.CharField(max_length = 64,
+               help_text = 'The author\'s position.')
